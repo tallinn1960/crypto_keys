e9a35cfcbb7f9988eee3003a6d5027d5da4a3f03
Olaf Schlüter <tallinn1960@yahoo.de>
2022-03-15 16:19:52 +0100

    Version 0.3.2: Added  BP-256 as curve identifier

--- lib/src/keys.dart
+++ lib/src/keys.dart
@@ -182,7 +182,8 @@ Identifier _parseCurve(String name) {
     'P-256K': curves.p256k,
     'P-384': curves.p384,
     'P-521': curves.p521,
-    'BP-256R1': curves.bp256r1
+    'BP-256R1': curves.bp256r1,
+    'BP-256': curves.bp256r1
   }[name];
   if (v == null) {
     throw UnsupportedError('Unknown curve $name');
